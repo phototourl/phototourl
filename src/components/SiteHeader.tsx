67a2bbb15afe509984a2c2f7aacddacc29e34ca8
liqs <553139194@qq.com>
2025-12-12 07:55:49 +0800

    fix: point header logo to existing asset

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/image/logo_draft_4.png";
+import logo from "@/image/logo.png";
 
 export function SiteHeader() {
   return (
